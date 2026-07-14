54dd1c5c560951f262ec9d69f44bfc7c3f71709f
sihnat24 <sihnat2002@gmail.com>
2026-07-14 17:06:00 -0400

    implemented matmul, and transpose

---
+++ src/tensor.hpp
@@ -0,0 +1,82 @@
+#pragma once
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <cmath>
+
+class Tensor {
+public:
+    int rows;
+    int cols;
+    std::vector<float> data;
+
+    Tensor(int rows, int cols)
+        : rows(rows), cols(cols), data(rows * cols, 0.0f)
+    {}
+
+    float& at(int i, int j) {
+        return data[i * cols + j];
+    }
+
+    const float& at(int i, int j) const {
+        return data[i * cols + j];
+    }
+
+    static Tensor from_file(const std::string& path, int rows, int cols) {
+        Tensor v(rows, cols);
+        std::ifstream f(path, std::ios::binary);
+        if (!f) throw std::runtime_error("Cannot open: " + path);
+        f.read(reinterpret_cast<char*>(v.data.data()), rows * cols * sizeof(float));
+        return v;
+    }
+
+    Tensor add(const Tensor& a, const Tensor& b){
+
+        if (a.rows != b.rows || a.cols != b.cols) {
+            throw std::invalid_argument("Tensor dimension mismatch");
+        }
+        
+        Tensor out(a.rows, a.cols);
+        
+        for (int i = 0; i < a.rows; i++){
+            for (int j = 0; j < a.cols; j++){
+                out.at(i,j) = a.at(i,j) + b.at(i,j);
+            }
+        }
+        return out;
+    }
+
+    //Tensor transpose
+
+    Tensor matmul(const Tensor& other){
+       if (cols != other.rows){
+        throw std::invalid_argument("Tensor dimension mismatch");
+       } 
+
+       Tensor m(rows,other.cols); //create new tensor
+       
+       float dp;
+
+       for (int i = 0; i < rows; i++){
+        for (int j = 0; j < other.cols; j++){
+            dp = 0.0;
+            for (int p = 0; p < cols; p++){ //shared dim
+                dp += this->at(i,p) * other.at(p,j);
+            }
+            m.at(i,j) = dp;
+        }
+       }
+       return m;
+    }
+
+    Tensor transpose() const{
+        Tensor t(cols,rows);
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                t.at(j,i) = this->at(i,j);
+            }
+        }
+        return t;
+    }
+};
